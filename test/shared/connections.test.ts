050567a1e6bec06b42fa10a84f027f4a3993de25
Andreas Andreou <aandreou@prodly.co>
2024-01-02 14:34:51 +0200

    [PM-5283] Add create connection unit test

--- test/shared/connections.test.ts
+++ test/shared/connections.test.ts
@@ -2,6 +2,7 @@ import { AuthInfo, Connection, Org } from '@salesforce/core';
 import { expect } from 'chai';
 import sinon, { SinonSandbox, SinonStub } from 'sinon';
 import {
+  createConnection,
   getConnectionId,
   queryConnection,
   queryConnections,
@@ -12,8 +13,21 @@ describe('connections service', () => {
   let sandbox: SinonSandbox;
   let hubConnStub: SinonStub;
 
+  let determineIfScratchStub: SinonStub;
+  let determineIfSandboxStub: SinonStub;
+  let getUsernameStub: SinonStub;
+  let getOrgIdStub: SinonStub;
+  let getConnectionStub: SinonStub;
+
   beforeEach(() => {
     sandbox = sinon.createSandbox();
+
+    // Stub the Org methods
+    determineIfScratchStub = sandbox.stub(Org.prototype, 'determineIfScratch');
+    determineIfSandboxStub = sandbox.stub(Org.prototype, 'determineIfSandbox');
+    getUsernameStub = sandbox.stub(Org.prototype, 'getUsername');
+    getOrgIdStub = sandbox.stub(Org.prototype, 'getOrgId');
+    getConnectionStub = sandbox.stub(Org.prototype, 'getConnection');
 
     // Stub the Connection and Org class methods
     hubConnStub = sandbox.stub(Connection.prototype, 'query');
@@ -21,6 +35,52 @@ describe('connections service', () => {
 
   afterEach(() => {
     sandbox.restore();
+  });
+
+  it('should create a connection', async () => {
+    const connectionMock = {
+      // eslint-disable-next-line camelcase
+      PDRI__Active__c: true,
+      Name: 'testUsername testOrgId',
+      // eslint-disable-next-line camelcase
+      PDRI__OrganizationId__c: 'testOrgId',
+      // eslint-disable-next-line camelcase
+      PDRI__Access_Token__c: 'testAccessToken',
+      // eslint-disable-next-line camelcase
+      PDRI__Org_Type__c: 'Production',
+      // eslint-disable-next-line camelcase
+      PDRI__Instance_URL__c: 'testInstanceUrl',
+      // eslint-disable-next-line camelcase
+      PDRI__User_Id__c: 'testUserId',
+      // eslint-disable-next-line camelcase
+      PDRI__Username__c: 'testUsername',
+    };
+
+    determineIfScratchStub.resolves(false);
+    determineIfSandboxStub.resolves(false);
+    getUsernameStub.returns('testUsername');
+    getOrgIdStub.returns('testOrgId');
+    getConnectionStub.returns({
+      getConnectionOptions: () => ({
+        accessToken: 'testAccessToken',
+        instanceUrl: 'testInstanceUrl/',
+        userId: 'testUserId',
+      }),
+    });
+
+    hubConnStub.resolves({ id: 'testId' });
+
+    const createStub = sandbox.stub(Connection.prototype, 'create');
+    createStub.resolves({ success: true, id: 'testId', errors: [] });
+
+    const connectionId = await createConnection({
+      name: undefined,
+      org: new Org(),
+      hubConn: new Connection({ authInfo: new AuthInfo() }),
+    });
+
+    expect(connectionId).to.equal('testId');
+    expect(createStub.calledOnceWith('PDRI__Connection__c', connectionMock)).to.be.true;
   });
 
   it('should get a connection id', async () => {
